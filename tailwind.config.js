80cfc4f9b5ce40dd031284f075a4ac6d1a96333c
Elioth <dharsuthdra@gmail.com>
2024-06-28 00:05:50 +0800

    Commit Pertama

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [".//**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "ungu": "#5D50C6",
+        "pink": "#F85E9F",
+        "orange": "#FF7522",
+        "grey": "#222831",
+        "pastel": "#FACD49"
+      },
+      dropShadow:{
+      }
+    },
+  },
+  plugins: [],
+}
+
